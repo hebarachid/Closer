eb56c57962e8308e46e2d5033064dc35e3bbf240
hebarachid <67964217+hebarachid@users.noreply.github.com>
2021-12-03 00:45:05 +0200

    Initial commit

---
+++ app/src/main/java/com/example/closer/design4.java
@@ -0,0 +1,14 @@
+package com.example.closer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class design4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_design4);
+    }
+}
