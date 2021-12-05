947f4238da15e9d49849a36af2e2b2a368ea7a71
hebarachid <67964217+hebarachid@users.noreply.github.com>
2021-12-06 00:03:50 +0200

    finally, was able to register into the firebase

--- app/src/main/java/com/example/closer/Signup.java
+++ app/src/main/java/com/example/closer/Signup.java
@@ -1,14 +1,66 @@
 package com.example.closer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Signup extends AppCompatActivity {
-
+   DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference()
+           ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        final EditText Name=findViewById(R.id.Name);
+        final EditText Email=findViewById(R.id.EmailSignup);
+        final EditText Password=findViewById(R.id.PasswordSignup);
+        final ImageButton Register=findViewById(R.id.Register);
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String nameTxt=Name.getText().toString();
+                final String emailTxt=Email.getText().toString();
+                final String passwordTxt=Password.getText().toString();
+
+                if(nameTxt.isEmpty()||emailTxt.isEmpty()||passwordTxt.isEmpty()){
+                    Log.i("he","he");
+                }
+                else{
+                    databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.hasChild(emailTxt)){
+                                Log.i("reg","already");
+                            }else{
+                                databaseReference.child("users").child(emailTxt).child("Name").setValue(nameTxt);
+                                databaseReference.child("users").child(emailTxt).child("Password").setValue(passwordTxt);
+
+                                Log.i("success","success");
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/closer/login.java
+++ app/src/main/java/com/example/closer/login.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class login extends AppCompatActivity {
@@ -13,6 +16,26 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final EditText email= findViewById(R.id.Email);
+        final EditText password=findViewById(R.id.Password);
+        final ImageButton loginBtn=findViewById(R.id.login);
+        final ImageButton signupBtn=findViewById(R.id.signup);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String emailTxt=email.getText().toString();
+                final String passwordTxt=password.getText().toString();
+
+                if(emailTxt.isEmpty()||passwordTxt.isEmpty()){
+                    Log.i("empty","empty");
+                }
+                else{
+
+                }
+
+            }
+        });
     }
     public void directSignUp(View view){
         Intent intent = new Intent(this, Signup.class);
