ec56b9ba0ba127cd96c825b9909e8bc76c7e6979
hebarachid <67964217+hebarachid@users.noreply.github.com>
2021-12-12 10:11:11 +0200

    movie Suggestion API, ADAPTER VIEW..

--- app/src/main/java/com/example/closer/MovieDisplayer.java
+++ app/src/main/java/com/example/closer/MovieDisplayer.java
@@ -1,6 +1,8 @@
 package com.example.closer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -9,70 +11,95 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MovieDisplayer extends AppCompatActivity {
-    List<String> MovieList = new ArrayList<String>();
+    List<MovieModelClass> movieList;
+    RecyclerView recyclerView;
+    String apiURL="https://api.themoviedb.org/3/movie/popular?api_key=4a56b975740745ae46b79bb11a7271e4";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_displayer);
+        movieList=new ArrayList<>();
+        recyclerView=findViewById(R.id.recyclerView);
 
-        String apiURL="https://api.themoviedb.org/3/movie/popular?api_key=4a56b975740745ae46b79bb11a7271e4";
-        DownloadTask task=new DownloadTask();
-        task.execute(apiURL);
+        GetData getData=new GetData();
+        getData.execute();
     }
-    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+    public class GetData extends AsyncTask<String,String,String>{
 
         @Override
-        protected String doInBackground(String... urls) {
-            String result = "";
-            URL url;
-            HttpURLConnection urlConnection;
-            try {
-                url = new URL(urls[0]);
-                urlConnection = (HttpURLConnection) url.openConnection();
-                //read
-                InputStream in = urlConnection.getInputStream();
-                InputStreamReader reader = new InputStreamReader(in);
-                int data = reader.read();
-                while (data != -1) {
-                    char current = (char) data;
-                    result += current;
-                    data = reader.read();
-                }
-                Log.i("result", "doInBackground: "+result);
-                return result;
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
+        protected String doInBackground(String... strings) {
+            String current="";
+            try{
+                URL url;
+                HttpURLConnection urlConnection=null;
+                try{
+                    url=new URL(apiURL);
+                    urlConnection=(HttpURLConnection) url.openConnection();
 
+                    InputStream is=urlConnection.getInputStream();
+                    InputStreamReader isr=new InputStreamReader(is);
+
+                    int data= isr.read();
+                    while (data!=-1){
+                        current+=(char)data;
+                        data=isr.read();
+                    }
+                    return current;
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    if(urlConnection!=null){
+                        urlConnection.disconnect();
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return current;
         }
 
         @Override
         protected void onPostExecute(String s) {
             try{
-                JSONObject jsonObject =new JSONObject(s);
+                JSONObject jsonObject=new JSONObject(s);
                 JSONArray jsonArray=jsonObject.getJSONArray("results");
                 for(int i=0;i<jsonArray.length();i++){
                     JSONObject jsonObject1=jsonArray.getJSONObject(i);
-                    String id= jsonObject1.getString("original_title");
-                    Log.i("hehe", "onPostExecute: "+id);
-              MovieList.add(id);
+                    MovieModelClass model=new MovieModelClass();
+                    model.setId(jsonObject1.getString("vote_average"));
+                    model.setName(jsonObject1.getString("title"));
+                    model.setImg(jsonObject1.getString("poster_path"));
+
+                    movieList.add(model);
                 }
-                ListView list = (ListView) findViewById(R.id.listView);
-                ArrayAdapter<String> arr=new ArrayAdapter<String>(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,MovieList);
-                list.setAdapter(arr);
-            }catch (Exception e) {
+
+            }catch (JSONException e){
                 e.printStackTrace();
+            }
+            putDataIntoRecyclerView( movieList);
         }
     }
-}}
+
+    private void putDataIntoRecyclerView(List<MovieModelClass> movieList){
+        MovieAdapter movieAdapter=new MovieAdapter(this,movieList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.setAdapter(movieAdapter);
+    }
+}
---
+++ app/src/main/java/com/example/closer/MovieModelClass.java
@@ -0,0 +1,41 @@
+package com.example.closer;
+
+public class MovieModelClass {
+
+    String id;
+    String name;
+    String img;
+
+    public MovieModelClass(String id, String name, String img) {
+        this.id = id;
+        this.name = name;
+        this.img = img;
+    }
+
+    public MovieModelClass() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
